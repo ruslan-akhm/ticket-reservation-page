0b5958aad1ab5b198929289bab6928692f61c972
Glitch (starter-cra-and-express) <none>
2020-07-31 14:21:25 +0000

    🗼😬 Checkpoint ./src/App.js:8816891/319 ./src/components/seats.js:8816891/2822

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ticketService from './services/ticketService'
+import Seats from './components/seats'
 import './App.css';
 
-const seatsLayout = {
-  A:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  B:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  C:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  D:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  E:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  F:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  G:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-  H:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
-};
+// const seatsLayout = {
+//   A:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   B:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   C:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   D:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   E:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   F:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   G:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+//   H:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+// };
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -66,30 +67,30 @@ class App extends React.Component{
     // update();
   }
   
- saveChosenSeats(e){
-   e.preventDefault();
-   document.getElementById('modal').style.display="block"
-   var allSeats = document.getElementsByClassName('check-box')
-   var seatP = [];
-   for(let x=0;x<allSeats.length;x++){
-     if(allSeats[x].checked==true){
-     seatP.push(allSeats[x].value)
-     }
-   }
-   console.log(seatP, seatP.length, typeof seatP)
-   var params = 'seat='+JSON.stringify(seatP) // We have to stringify to send to server and there we Parse to properly read the seats
-   console.log(params)
-    var newSave = new XMLHttpRequest();
-    newSave.open('POST', '/api/reserve', true);
-    newSave.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//  saveChosenSeats(e){
+//    e.preventDefault();
+//    document.getElementById('modal').style.display="block"
+//    var allSeats = document.getElementsByClassName('check-box')
+//    var seatP = [];
+//    for(let x=0;x<allSeats.length;x++){
+//      if(allSeats[x].checked==true){
+//      seatP.push(allSeats[x].value)
+//      }
+//    }
+//    console.log(seatP, seatP.length, typeof seatP)
+//    var params = 'seat='+JSON.stringify(seatP) // We have to stringify to send to server and there we Parse to properly read the seats
+//    console.log(params)
+//     var newSave = new XMLHttpRequest();
+//     newSave.open('POST', '/api/reserve', true);
+//     newSave.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
    
-    newSave.onload = function(){
-      var res = JSON.parse(this.response)
-      document.getElementById('top-line').innerHTML = res.text
-      document.getElementById('bottom-line').innerHTML = res.ticketId
-    }
-    newSave.send(params);
-  } 
+//     newSave.onload = function(){
+//       var res = JSON.parse(this.response)
+//       document.getElementById('top-line').innerHTML = res.text
+//       document.getElementById('bottom-line').innerHTML = res.ticketId
+//     }
+//     newSave.send(params);
+//   } 
   
   showOrCancelSeats(evt){
     evt.preventDefault();
@@ -126,16 +127,18 @@ class App extends React.Component{
   
   render(){
     //const takenSeats = this.state.taken;
-    const keys = Object.keys(seatsLayout);
-    const rows = keys.map(key=>{return <ul className="list" key={key}><li>row {key}</li></ul>})
-    this.seats = keys.map(row=>{return seatsLayout[row].map(seat=>{
-      return <label for={seat} key={row+''+seat} className="seat-label"><input className="check-box" id={row+""+seat} key={row+""+seat} type="checkbox" name="seat" value={row+""+seat}/>{seat}</label>
-    })});
+    
+    // const keys = Object.keys(seatsLayout);
+    // const rows = keys.map(key=>{return <ul className="list" key={key}><li>row {key}</li></ul>})
+    // this.seats = keys.map(row=>{return seatsLayout[row].map(seat=>{
+    //   return <label for={seat} key={row+''+seat} className="seat-label"><input className="check-box" id={row+""+seat} key={row+""+seat} type="checkbox" name="seat" value={row+""+seat}/>{seat}</label>
+    // })});
     
     return(
       <div id="page">
         <div id="stage">STAGE/SCREEN</div>
-        <div>
+        <Seats />
+        {/* <!--         <div>
         <form id="form1" onSubmit={this.saveChosenSeats}>
           <div id="parent"> 
             <div id="seats-rows">{rows}</div>
@@ -143,7 +146,7 @@ class App extends React.Component{
             <input className="reserve" id="reserve-button" type="submit" value="Reserve"></input>
           </div>
         </form>
-        </div>
+        </div> --> */}
         <div id="have-id">Already made reservation? To check your seats enter your ticket ID
           <form id="form2" onSubmit={this.showOrCancelSeats}>
             <input id="text-field" type="text" name="id" placeholder="enter your ticket ID" required></input>
---
+++ src/components/seats.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+
+const seatsLayout = {
+  A:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  B:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  C:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  D:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  E:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  F:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  G:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+  H:["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+};
+
+function Seats(){
+  
+  const saveChosenSeats=(e)=>{
+   e.preventDefault();
+   document.getElementById('modal').style.display="block"
+   var allSeats = document.getElementsByClassName('check-box')
+   var seatP = [];
+   for(let x=0;x<allSeats.length;x++){
+     if(allSeats[x].checked==true){
+     seatP.push(allSeats[x].value)
+     }
+   }
+   console.log(seatP, seatP.length, typeof seatP)
+   var params = 'seat='+JSON.stringify(seatP) // We have to stringify to send to server and there we Parse to properly read the seats
+   console.log(params)
+    var newSave = new XMLHttpRequest();
+    newSave.open('POST', '/api/reserve', true);
+    newSave.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+   
+    newSave.onload = function(){
+      var res = JSON.parse(this.response)
+      document.getElementById('top-line').innerHTML = res.text
+      document.getElementById('bottom-line').innerHTML = res.ticketId
+    }
+    newSave.send(params);
+  } 
+  
+  const keys = Object.keys(seatsLayout);
+  const rows = keys.map(key=>{return <ul className="list" key={key}><li>row {key}</li></ul>})
+  const seats = keys.map(row=>{return seatsLayout[row].map(seat=>{
+      return <label for={seat} key={row+''+seat} className="seat-label"><input className="check-box" id={row+""+seat} key={row+""+seat} type="checkbox" name="seat" value={row+""+seat}/>{seat}</label>
+    })});
+  
+  return(
+    <div>
+      <form id="form1" onSubmit={saveChosenSeats}>
+        <div id="parent"> 
+          <div id="seats-rows">{rows}</div>
+          <div id="seats-parent">{seats}</div>
+          <input className="reserve" id="reserve-button" type="submit" value="Reserve"></input>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Seats
