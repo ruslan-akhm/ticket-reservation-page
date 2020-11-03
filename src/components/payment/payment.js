eb7bf5921b5e69edf2d90be9e2799e9ed960cc56
Glitch (starter-cra-and-express) <none>
2020-11-03 14:00:33 +0000

    🍐👳 Checkpoint ./src/components/payment/payment.js:8816891/1624

--- src/components/payment/payment.js
+++ src/components/payment/payment.js
@@ -1,8 +1,22 @@
-import React from "react"
-import "./payment.scss"
+import React, { useState, useContext, useEffect } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { SeatsContext } from "../../context/seatsContext";
+import "./payment.scss";
 
-function Payment(){
-  return <div>PAYMENT</div>
+function Payment() {
+  const { total, setTotal } = useContext(SeatsContext);
+  
+  console.log(typeof total)
+  
+  const handleToken = (token) => {
+    
+  }
+  
+  return (
+    <div>
+      <StripeCheckout stripeKey={process.env.STRIPE_KEY} token={handleToken} amount={total}/>
+    </div>
+  );
 }
 
 export default Payment;
