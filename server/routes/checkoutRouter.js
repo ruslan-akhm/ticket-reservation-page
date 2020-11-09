b75e4bb7bebda6554d0cb02bc8cd6934ebfcad2c
Glitch (starter-cra-and-express) <none>
2020-11-09 03:13:20 +0000

    🖼💴 Checkpoint ./server/routes/checkoutRouter.js:8816891/6382

--- server/routes/checkoutRouter.js
+++ server/routes/checkoutRouter.js
@@ -10,45 +10,59 @@ checkoutRouter.post("/", async (req, res) => {
   let error;
   let status;
   try {
+    //     const { product, id, price, user } = req.body;
+    //     let tickets = [];
+    //     for (let i = 0; i < product.length; i++) {
+    //       tickets.push(product[i].id);
+    //     }
+    // //where to grab a token?
+    //     const customer = await stripe.customers.create({
+    //       email: user.email,
+    //       name: user.name
+    //     });
+
+    //     const idempotencyKey = shortid.generate();
+    //     const charge = await stripe.charges.create(
+    //       {
+    //         amount: price,
+    //         currency: "cad",
+    //         customer: customer.id,
+    //         //customer: customer.id,
+    //         //receipt_email: token.email,
+    //         description: `Purchased ${tickets.length} ticket(s): ${tickets}. Customer: ${customer.id}`
+    //         // shipping: {
+    //         //   name: token.card.name,
+    //         //   address: {
+    //         //     line1: token.card.address_line1,
+    //         //     line2: token.card.address_line2,
+    //         //     city: token.card.address_city,
+    //         //     country: token.card.address_country,
+    //         //     postal_code: token.card.address_zip
+    //         //   }
+    //         // }
+    //       },
+    //       {
+    //         idempotencyKey
+    //       }
+    //     );
+    //console.log("Charge:", { charge });
     const { product, id, price, user } = req.body;
     let tickets = [];
     for (let i = 0; i < product.length; i++) {
       tickets.push(product[i].id);
     }
-//where to grab a token? 
-    const customer = await stripe.customers.create({
-      email: user.email,
-      name: user.name
+
+    const payment = stripe.paymentIntents.create({
+      amount: price,
+      currency: "CAD",
+      description: `Purchased ${tickets.length} ticket(s): ${tickets}.`,
+      payment_method: id,
+      confirm: true
     });
 
-    const idempotencyKey = shortid.generate();
-    const charge = await stripe.charges.create(
-      {
-        amount: price,
-        currency: "cad",
-        customer: customer.id,
-        //customer: customer.id,
-        //receipt_email: token.email,
-        description: `Purchased ${tickets.length} ticket(s): ${tickets}. Customer: ${customer.id}`
-        // shipping: {
-        //   name: token.card.name,
-        //   address: {
-        //     line1: token.card.address_line1,
-        //     line2: token.card.address_line2,
-        //     city: token.card.address_city,
-        //     country: token.card.address_country,
-        //     postal_code: token.card.address_zip
-        //   }
-        // }
-      },
-      {
-        idempotencyKey
-      }
-    );
-    //console.log("Charge:", { charge });
-    console.log("Charge:");
+    console.log("Charge:", payment);
     status = "success";
-    error=false
+    error = false;
     console.log(tickets);
     for (let x = 0; x < tickets.length; x++) {
       Seat.updateOne(
@@ -59,24 +73,22 @@ checkoutRouter.post("/", async (req, res) => {
         }
       );
     }
-    
+
     //TO USE UPDATE MANY:
     //have to clean userId on front-end whenever user presses CANCEL or PURCHASE and its success - have to anyway
-    
-    
-      // Seat.updateMany(
-      //   { userId: user },
-      //   { $set: { isTaken: true } },
-      //   (err, data) => {
-      //     if (err) return console.log(err);
-      //     if (!data) console.log("NO TICKET");
-      //   }
-      // );
-    
+
+    // Seat.updateMany(
+    //   { userId: user },
+    //   { $set: { isTaken: true } },
+    //   (err, data) => {
+    //     if (err) return console.log(err);
+    //     if (!data) console.log("NO TICKET");
+    //   }
+    // );
   } catch (error) {
     console.error("Error:", error);
     status = "failure";
-    error=true;
+    error = true;
   }
 
   res.json({ error: error, status });
